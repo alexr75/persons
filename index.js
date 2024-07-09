8ab79fb1582b9ceb747bb30766374f6f151616fb
alex75 <alex@alexrobinson.id.au>
2024-07-09 16:10:44 +0800

    add static server  to express

--- index.js
+++ index.js
@@ -92,6 +92,8 @@ app.delete('/api/persons/:id', (req, res) => {
 	res.status(204).end()
 })
 
+app.use(express.static('dist'))
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
