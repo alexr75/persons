a12466e24293569949a2cce92aa3bff7d767b2d3
alex75 <alex@alexrobinson.id.au>
2024-07-12 10:27:16 +0800

    Added lint and fixed stylistic issues

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const mongoose = require('mongoose')
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -11,97 +9,97 @@ app.use(cors())
 
 app.use(express.json())
 
-morgan.token('json', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('json', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 
 app.get('/api/persons', (req, res) => {
-	Person.find({}).then(people => {
-		res.json(people)
-	})
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
 app.get('/info', (req, res) => {
-	const date = new Date()
-	Person.countDocuments({}).then(count => {
-		res.send(`
+  const date = new Date()
+  Person.countDocuments({}).then(count => {
+    res.send(`
 			<p>Phonebook has info for ${count} people</p>
 			<p>${date}</p>
 		`)
-	})
+  })
 
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-	Person.findById(req.params.id)
-		.then(person => {
-			if (person) {
-				res.json(person)
-			} else {
-				res.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons/', (req, res, next) => {
-	const { name, number } = req.body
-	Person.exists({ name }).then((exists) => {
-		if (exists) {
-			return res.status(400).json({
-				error: 'name must be unique'
-			})
-		} else {
-			const person = new Person({ name, number })
-			person.save()
-				.then(savedPerson => {
-					res.json(savedPerson)
-				})
-				.catch(error => next(error))
-		}
-	})
+  const { name, number } = req.body
+  Person.exists({ name }).then((exists) => {
+    if (exists) {
+      return res.status(400).json({
+        error: 'name must be unique'
+      })
+    } else {
+      const person = new Person({ name, number })
+      person.save()
+        .then(savedPerson => {
+          res.json(savedPerson)
+        })
+        .catch(error => next(error))
+    }
+  })
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-	const { name, number } = req.body
+  const { name, number } = req.body
 
-	Person.findByIdAndUpdate(
-			req.params.id, 
-			{ name, number }, 
-			{ new: true, runValidators: true, context: 'query' }
-		)
-		.then(updatedPerson => {
-			if (updatedPerson) {
-				res.json(updatedPerson)
-			} else {
-				res.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        res.json(updatedPerson)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-	Person.findByIdAndDelete(req.params.id)
-		.then((result) => {
-			res.status(204).end()
-		})
-		.catch(error => next(error))
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.use(express.static('dist'))
 
 const errorHandler = (error, req, res, next) => {
-	console.error(error.message)
+  console.error(error.message)
 
-	if (error.name === 'CastError') {
-		return res.status(400).send({ error: 'malformatted id' })
-	} else if (error.name === 'ValidationError') {
-		return res.status(400).json({ error: error.message })
-	}
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-	next(error)
+  next(error)
 }
 app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server running on port ${process.env.PORT}`)
+  console.log(`Server running on port ${process.env.PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -6,26 +6,26 @@ mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URL)
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length > 3) {
-	const name = process.argv[2]
-	const number = process.argv[3]	
-	const person = new Person({name, number})
-	person.save().then(result => {
-		console.log(`added ${name} ${number} to phonebook`)
-		mongoose.connection.close()
-	})
+  const name = process.argv[2]
+  const number = process.argv[3]
+  const person = new Person({ name, number })
+  person.save().then(() => {
+    console.log(`added ${name} ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-	Person.find({}).then(people => {
-		console.log('phonebook:')
-		people.forEach(person => {
-			console.log(person.name,person.number)
-		})
-		mongoose.connection.close()
-	})
+  Person.find({}).then(people => {
+    console.log('phonebook:')
+    people.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
 }
